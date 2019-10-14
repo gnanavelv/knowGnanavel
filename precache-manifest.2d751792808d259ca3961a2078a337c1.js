7dc73a715d63e9a3cc5cfbbbf433ab9d392bb261
gnanavelv <gnanavelv27@gmail.com>
2019-10-14 19:09:14 +0530

    Updates

---
+++ precache-manifest.2d751792808d259ca3961a2078a337c1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80c6de9944d053c1de622e52d525593",
+    "url": "/knowGnanavel/index.html"
+  },
+  {
+    "revision": "1bf2f63b76745fafba50",
+    "url": "/knowGnanavel/static/css/main.c93324c0.chunk.css"
+  },
+  {
+    "revision": "749eb8c932665470c653",
+    "url": "/knowGnanavel/static/js/2.9fc1e76c.chunk.js"
+  },
+  {
+    "revision": "1bf2f63b76745fafba50",
+    "url": "/knowGnanavel/static/js/main.f117d3ab.chunk.js"
+  },
+  {
+    "revision": "0d1d9c4e755d4390a397",
+    "url": "/knowGnanavel/static/js/runtime-main.95385ee0.js"
+  },
+  {
+    "revision": "103f0b6715184a3f81dddef15fffbefa",
+    "url": "/knowGnanavel/static/media/Resume.103f0b67.pdf"
+  },
+  {
+    "revision": "c5bdbb4e07d9f6c67b8fafa970297599",
+    "url": "/knowGnanavel/static/media/bars.c5bdbb4e.svg"
+  },
+  {
+    "revision": "5466ba93750871455ef4d35ec403eb65",
+    "url": "/knowGnanavel/static/media/colors.5466ba93.svg"
+  },
+  {
+    "revision": "c3e807114a9ea95a13d2ca68a8bd39d9",
+    "url": "/knowGnanavel/static/media/core.c3e80711.svg"
+  },
+  {
+    "revision": "b946eac6d3a4ebf5ed0f92c1457f0d7f",
+    "url": "/knowGnanavel/static/media/dev.b946eac6.svg"
+  },
+  {
+    "revision": "9f68ac536c73db112707dde8a3614ece",
+    "url": "/knowGnanavel/static/media/odm.9f68ac53.png"
+  },
+  {
+    "revision": "57c7732322dfe6b494d3cf67e6600478",
+    "url": "/knowGnanavel/static/media/realtors.57c77323.png"
+  },
+  {
+    "revision": "757d12d0d59789feb17d3018c7db3e5e",
+    "url": "/knowGnanavel/static/media/web.757d12d0.svg"
+  },
+  {
+    "revision": "5e89517c7d20a66b27a1c06813ebce42",
+    "url": "/knowGnanavel/static/media/wordpress.5e89517c.png"
+  }
+]);
